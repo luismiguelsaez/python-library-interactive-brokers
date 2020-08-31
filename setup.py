ba171e664eb3d08f3e500d52215a978137f56ce1
luismiguelsaez <luismiguelsaez@protonmail.com>
2020-08-31 18:23:36 +0200

    Added requirements

--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
 import setuptools
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+
 setuptools.setup(
      name='python_ib',  
      version='0.1',
-     #scripts=['dokr'] ,
      author="Luis Miguel Sáez",
      author_email="luismiguelsaez83@gmail.com",
      description="Interactive Brokers flex query download",
      url="https://github.com/luismiguelsaez/python-library-interactive-brokers/script",
      packages=setuptools.find_packages(),
+     install_reqs = parse_requirements('requirements.txt', session='hack'),
      classifiers=[
          "Programming Language :: Python :: 3",
          "License :: OSI Approved :: MIT License",
